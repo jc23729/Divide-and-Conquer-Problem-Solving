06af9630123e3d8d7719c63a8b3c2a9c4f86e1e0
Juan Cruz <jc23729@gmail.com>
2021-02-28 12:04:43 -0500

    linear search

--- binarySearch.js
+++ binarySearch.js
@@ -4,10 +4,27 @@
 // Explain how divide and conquer strategies improve time complexity
 
 
-function linearSearch(arr, target) {
-    for (let i = 0; i < arr.length; i++ ){
-        if(arr[i] === target) return i;
-    }
-    return -1;
-}
+// Linear Search
+// How is indexOf implemented in JavaScript?
 
+// Loop through your array looking for the desired value
+// If you find it, return the index
+// If you exhaust the array, return -1
+// Time complexity: O(n)
+
+
+// custom indexOf implementation
+function indexOf(arr, val) {
+
+    for (let i = 0; i < arr.length; i++) {
+      // if you find the value, return the index
+      if (arr[i] === val) return i;
+    }
+  
+    // if you never find the value, return -1
+    return -1;
+  }
+  
+  indexOf([4, 8, 15, 16, 23, 42], 15); // 2
+  indexOf([4, 8, 15, 16, 23, 42], 42); // 5
+  indexOf([4, 8, 15, 16, 23, 42], 100); // -1
