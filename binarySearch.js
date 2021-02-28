e73c58576bd61a882a98df7ab230b2ac474506e6
Juan Cruz <jc23729@gmail.com>
2021-02-28 12:00:32 -0500

    Goals of divide and conquer

---
+++ binarySearch.js
@@ -0,0 +1,6 @@
+// Goals
+// Describe what a divide and conquer strategy is
+// Discuss when it’s possible to use divide and conquer, and when not
+// Explain how divide and conquer strategies improve time complexity
+
+
