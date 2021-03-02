087a6fb6d72e2370bf5a34f03876f0920fc1cf85
Juan Cruz <jc23729@gmail.com>
2021-03-01 21:01:00 -0500

    update

--- binarySearch.js
+++ binarySearch.js
@@ -99,3 +99,11 @@ function binarySearch(arr, val) {
 //     # Driver Code 
 // arr = [ 2, 3, 4, 10, 40 ] 
 // x = 10
+
+// # Function call 
+// result = binarySearch(arr, 0, len(arr)-1, x) 
+  
+// if result != -1: 
+//     print ("Element is present at index % d" % result) 
+// else: 
+//     print ("Element is not present in array") 
