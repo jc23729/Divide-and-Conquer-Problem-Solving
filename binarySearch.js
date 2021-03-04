a360ea0f3f1dbde4c49e18eb73f838b7bd72d1b7
Juan Cruz <jc23729@gmail.com>
2021-03-03 21:50:23 -0500

    update

--- binarySearch.js
+++ binarySearch.js
@@ -131,3 +131,9 @@ countZeroes([1,1,1,1]) // 0
 // Time Complexity: O(log N)
 
 // Examples:
+
+sortedFrequency([1,1,2,2,2,2,3],2) // 4
+sortedFrequency([1,1,2,2,2,2,3],3) // 1
+sortedFrequency([1,1,2,2,2,2,3],1) // 2
+sortedFrequency([1,1,2,2,2,2,3],4) // -1
+
