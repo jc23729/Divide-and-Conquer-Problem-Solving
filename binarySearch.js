44ddd8bbe488b20033fa6593d9f3f1b7e048a6bc
Juan Cruz <jc23729@gmail.com>
2021-03-03 21:52:05 -0500

    update

--- binarySearch.js
+++ binarySearch.js
@@ -146,4 +146,12 @@ sortedFrequency([1,1,2,2,2,2,3],4) // -1
 
 // Examples:
 
+findRotatedIndex([3,4,1,2],4) // 1
+findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 8) // 2
+findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 3) // 6
+findRotatedIndex([37,44,66,102,10,22],14) // -1
+findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 12) // -1
+
+
+
 
