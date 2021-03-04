16b5123b06256ef3239dd9eec674ece5d85801cc
Juan Cruz <jc23729@gmail.com>
2021-03-03 21:47:07 -0500

    update

--- binarySearch.js
+++ binarySearch.js
@@ -107,3 +107,7 @@ function binarySearch(arr, val) {
 //     print ("Element is present at index % d" % result) 
 // else: 
 //     print ("Element is not present in array") 
+
+// countZeroes
+// Given an array of 1s and 0s which has all 1s first followed by all 0s, write a function called countZeroes, which returns the number of zeroes in the array.
+
