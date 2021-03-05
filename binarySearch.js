dfe2d6abbcef58b55fd9ed39368b0668bc19dfad
Juan Cruz <jc23729@gmail.com>
2021-03-04 22:14:41 -0500

    update

--- binarySearch.js
+++ binarySearch.js
@@ -198,10 +198,10 @@ function binarySearch(arr, val) {
   let leftIdx = 0;
   let rightIdx = arr.length - 1;
 
-//   while (leftIdx <= rightIdx) {
-//     // find the middle value
-//     let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
-//     let middleVal = arr[middleIdx];
+  while (leftIdx <= rightIdx) {
+    // find the middle value
+    let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+    let middleVal = arr[middleIdx];
 
 //     if (middleVal < val) {
 //       // middleVal is too small, look at the right half
