aab487f88ac8f9ea07dcfa2f1f94a2cdbb13b2b8
Juan Cruz <jc23729@gmail.com>
2021-03-04 22:07:50 -0500

    update

--- binarySearch.js
+++ binarySearch.js
@@ -152,6 +152,20 @@ findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 3) // 6
 findRotatedIndex([37,44,66,102,10,22],14) // -1
 findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 12) // -1
 
+// custom indexOf implementation
+function indexOf(arr, val) {
 
+//   for (let i = 0; i < arr.length; i++) {
+//     // if you find the value, return the index
+//     if (arr[i] === val) return i;
+//   }
+
+//   // if you never find the value, return -1
+//   return -1;
+// }
+
+// indexOf([4, 8, 15, 16, 23, 42], 15); // 2
+// indexOf([4, 8, 15, 16, 23, 42], 42); // 5
+// indexOf([4, 8, 15, 16, 23, 42], 100); // -1
 
 
