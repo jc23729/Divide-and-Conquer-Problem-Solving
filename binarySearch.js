3e0a96dbd9db57729ee2b5ab28b6dcc058280d63
Juan Cruz <jc23729@gmail.com>
2021-03-04 22:15:01 -0500

    update

--- binarySearch.js
+++ binarySearch.js
@@ -203,17 +203,17 @@ function binarySearch(arr, val) {
     let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
     let middleVal = arr[middleIdx];
 
-//     if (middleVal < val) {
-//       // middleVal is too small, look at the right half
-//       leftIdx = middleIdx + 1;
-//     } else if (middleVal > val) {
-//       // middleVal is too large, look at the left half
-//       rightIdx = middleIdx - 1;
-//     } else {
-//       // we found our value!
-//       return middleIdx;
-//     }
-//   }
+    if (middleVal < val) {
+      // middleVal is too small, look at the right half
+      leftIdx = middleIdx + 1;
+    } else if (middleVal > val) {
+      // middleVal is too large, look at the left half
+      rightIdx = middleIdx - 1;
+    } else {
+      // we found our value!
+      return middleIdx;
+    }
+  }
   
 //   // left and right pointers crossed, val isn't in arr
 //   return -1;
