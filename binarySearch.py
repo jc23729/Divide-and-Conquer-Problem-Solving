0098d42b373b40db0d56831624634bd987ced3a4
Juan Cruz <jc23729@gmail.com>
2021-03-06 22:45:02 -0500

    update

--- binarySearch.py
+++ binarySearch.py
@@ -15,6 +15,16 @@
 # All the integers in nums are unique.
 # nums is sorted in an ascending order.
 
+# Approach 1: Binary Search
+# Intuition
+
+# Binary search is a textbook algorithm based on the idea to compare the target value to the middle element of the array.
+
+# If the target value is equal to the middle element - we're done.
+
+# If the target value is smaller - continue to search on the left.
+
+# If the target value is larger - continue to search on the right.
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
