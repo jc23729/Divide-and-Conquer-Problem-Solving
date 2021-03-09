eb271afbff1146ce82f1fe81bd700fdb37318754
Juan Cruz <jc23729@gmail.com>
2021-03-08 23:43:29 -0500

    update

--- binarySearch.py
+++ binarySearch.py
@@ -69,7 +69,7 @@ Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,
 
 Input: nums = [1,2,3,4,4,3,2,1], n = 4
 Output: [1,4,2,3,3,2,4,1]
-Example 3:
+# Example 3:
 
 Input: nums = [1,1,2,2], n = 2
 Output: [1,2,1,2]
