8b2786a58e7a0bbd39366758b3971eac065f9345
Juan Cruz <jc23729@gmail.com>
2021-03-08 23:39:21 -0500

    update

--- binarySearch.py
+++ binarySearch.py
@@ -54,3 +54,9 @@ class Solution:
             else:
                 left = pivot + 1
         return -1
+
+
+#     Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+
+# Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
